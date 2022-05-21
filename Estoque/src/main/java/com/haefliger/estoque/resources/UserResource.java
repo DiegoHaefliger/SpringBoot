dff9539a199f24953019bb9616623d571ebbf71c
DiegoHaefliger <diegohaefliger@hotmail.com>
2022-05-20 21:22:45 -0300

    add User

---
+++ Estoque/src/main/java/com/haefliger/estoque/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.haefliger.estoque.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.haefliger.estoque.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Diego", "123456789", "senha");
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
