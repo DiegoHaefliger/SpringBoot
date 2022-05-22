bea9ed28ceedee98e9f4b74dcf0d199eeebb7804
DiegoHaefliger <diegohaefliger@hotmail.com>
2022-05-22 17:18:31 -0300

    User findById

---
+++ Estoque/src/main/java/com/haefliger/estoque/services/UserServices.java
@@ -0,0 +1,27 @@
+package com.haefliger.estoque.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.haefliger.estoque.entities.User;
+import com.haefliger.estoque.repositories.UserRepository;
+
+@Service
+public class UserServices {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
