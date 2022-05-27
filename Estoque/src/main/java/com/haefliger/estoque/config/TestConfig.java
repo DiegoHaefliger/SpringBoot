0e57fc519ff98a6f5c393913873d998737068413
DiegoHaefliger <diegohaefliger@hotmail.com>
2022-05-26 21:32:15 -0300

    entities order_item

--- Estoque/src/main/java/com/haefliger/estoque/config/TestConfig.java
+++ Estoque/src/main/java/com/haefliger/estoque/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.haefliger.estoque.entities.Category;
 import com.haefliger.estoque.entities.Order;
+import com.haefliger.estoque.entities.OrderItem;
 import com.haefliger.estoque.entities.Product;
 import com.haefliger.estoque.entities.User;
 import com.haefliger.estoque.entities.enums.OrderStatus;
 import com.haefliger.estoque.repositories.CategoryRepository;
+import com.haefliger.estoque.repositories.OrderItemRepository;
 import com.haefliger.estoque.repositories.OrderRepository;
 import com.haefliger.estoque.repositories.ProductRepository;
 import com.haefliger.estoque.repositories.UserRepository;
@@ -33,6 +35,11 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository prodctRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +75,14 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
 	}
 
 }
